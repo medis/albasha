f865f7c64a7fab21ce208ed4da0dfb0b70780dec
medis <aubuka@gmail.com>
2017-04-16 11:30:14 +0100

    add deployer

---
+++ deploy.php
@@ -0,0 +1,104 @@
+<?php
+
+require 'recipe/laravel.php';
+
+$path = '/var/www/albasha';
+
+// Define a server for deployment.
+server('dev', 'audrius.io', 22)
+  ->user('deployer')
+  ->identityFile('~/.ssh/id_deployex.pub', '~/.ssh/id_deployex', '')
+  ->stage('development')
+  ->env('deploy_path', $path);
+
+// Set repository.
+set('repository', 'git@github.com:medis/albasha.git');
+
+// Path to composer.
+set('composer_command', 'composer');
+
+/**
+ * Returns webroot folder.
+ */
+env('release_webroot', function () {
+    return str_replace("\n", '', run("readlink {{deploy_path}}/release")) . '/webroot';
+});
+
+/**
+ * Release path
+ */
+env('release_path', function () {
+    return str_replace("\n", '', run("readlink {{deploy_path}}/release"));
+});
+
+/**
+ * Append webroot to default recipe
+ */
+// Laravel shared dirs
+set('shared_dirs', [
+    'webroot/storage/app',
+    'webroot/storage/framework/cache',
+    'webroot/storage/framework/sessions',
+    'webroot/storage/framework/views',
+    'webroot/storage/logs',
+]);
+// Laravel 5 shared file
+set('shared_files', ['webroot/.env']);
+// Laravel writable dirs
+set('writable_dirs', ['webroot/storage', 'webroot/vendor']);
+
+// Override original composer task.
+/**
+ * Installing vendors tasks.
+ */
+task('deploy:vendors', function () {
+  $composer = get('composer_command');
+
+  if (! commandExist($composer)) {
+    run("cd {{release_webroot}} && curl -sS https://getcomposer.org/installer | php");
+    $composer = 'php composer.phar';
+  }
+  $composerEnvVars = env('env_vars') ? 'export ' . env('env_vars') . ' &&' : '';
+  run("cd {{release_webroot}} && $composerEnvVars $composer {{composer_options}}");
+})->desc('Installing vendors');
+
+/**
+ * Make deployed files writable to www-data group.
+ */
+task('change_permissions', function() {
+  run("chmod -R g+w {{deploy_path}}/release/webroot");
+});
+//after('deploy', 'change_permissions');
+
+/**
+ * Run Laravel5 optimisation commands.
+ * Reference: http://sentinelstand.com/article/laravel-5-optimization-commands.
+ */
+task('optimise', function() {
+  cd('{{deploy_path}}/release/webroot');
+  run('php artisan optimize');
+  run('php artisan config:cache');
+  run('php artisan route:cache');
+});
+
+/**
+ * Migrate database.
+ */
+task('database:migrate', function() {
+  cd('{{deploy_path}}/release/webroot');
+  run("php artisan migrate --force");
+});
+
+// Deployment script.
+task('deploy', [
+  'deploy:prepare',
+  'deploy:release',
+  'deploy:update_code',
+  'deploy:vendors',
+  'deploy:shared',
+  'change_permissions',
+  'database:migrate',
+  'optimise',
+  'deploy:symlink',
+  'cleanup',
+])->desc('Deploy your project');
