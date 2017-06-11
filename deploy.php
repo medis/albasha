d9aae9492e929262652ea4598fd34fc89f00b6b0
medis <aubuka@gmail.com>
2017-06-11 14:53:44 +0100

    disable registrations

--- deploy.php
+++ deploy.php
@@ -7,6 +7,7 @@ require 'recipe/laravel.php';
 
 $path = '/var/www/albasha';
 
+$live_path = '~/laravel';
 
 set('repository', 'git@github.com:medis/albasha.git');
 set('git_tty', true); // [Optional] Allocate tty for git on first deployment
@@ -31,16 +32,24 @@ host('audrius.io')
     ->identityFile('~/.ssh/id_deployex')
     ->set('deploy_path', $path);
 
+host('160.153.162.24')
+    ->stage('production')
+    ->user('basra123456')
+    ->identityFile('~/.ssh/id_albasha')
+    ->set('deploy_path', $live_path)
+    ->set('writable_use_sudo', false)
+    ->set('writable_mode', 'chmod')
+    ->set('http_user', 'basra123456');
 
 // Tasks
 
-desc('Restart PHP-FPM service');
-task('php-fpm:restart', function () {
-    // The user must have rights for restart service
-    // /etc/sudoers: username ALL=NOPASSWD:/bin/systemctl restart php-fpm.service
-    run('sudo systemctl restart php7.1-fpm.service');
-});
-after('deploy:symlink', 'php-fpm:restart');
+// desc('Restart PHP-FPM service');
+// task('php-fpm:restart', function () {
+//     // The user must have rights for restart service
+//     // /etc/sudoers: username ALL=NOPASSWD:/bin/systemctl restart php-fpm.service
+//     run('sudo systemctl restart php7.1-fpm.service');
+// });
+// after('deploy:symlink', 'php-fpm:restart');
 
 // [Optional] if deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
